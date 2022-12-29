3b4338ed0e7f48365bae85a4d94ee59dbf2c93ef
fabianomalves <fabianomalves@proton.me>
2022-12-29 20:17:35 -0300

    alterado arquivo gitignore. Alterada primeira aula

--- src/ep01.py
+++ src/ep01.py
@@ -1,64 +1,62 @@
 import pandas as pd
 
 # Series
-int(4.3) 
-serie_receita = pd.Series( [1,4,10,2,100000, 200, None], name="receita" )
+serie_receita = pd.Series(data=[1, 4, 10, 1000, 200, None], name='receita')
 
 # Mostrando a série criada
-print("Nossa série:\n\n", serie_receita )
-print("Tipo da nossa série: ", type(serie_receita))
+print(serie_receita)
+print(type(serie_receita))
 
 # DataFrame
-dados = {"nome": ['Teo', "Nah", "Code", "Karla"],
-         "sobrenome": ["Calvo", "Ataide", "Show", "Mag"],
-         "idade": [28, 30, 32, 30]  }
 
-df_pessoas = pd.DataFrame( dados ) # tb_pessoa
+dados = {'nome': ['Fabiano', 'Moreira', 'Code', 'Ciudad'],
+        'sobrenome': ['Moreira', 'Alves', 'Code', 'Del Leste'],
+        'idade': [43, 30, 32, 120]}
 
-## Começando a aula...
+df_pessoas = pd.DataFrame(dados)
 
-# Lendo um csv...
-pathfile_csv = "/home/teo/Documentos/pessoais/projetos/ensino/projetos_twitch/meu_treino/PandasToTeo/data/tb_candidatura_2018.csv"
-df_candidatura = pd.read_csv( pathfile_csv, sep=";")
+print(df_pessoas)
+
+# Começando a Aula
+
+# Lendo um csv:
+path_file_candidatura = '/home/fabiano/Insync/fabiano.moreira.alves@gmail.com/Google Drive/Projects/class_pandas/data/tb_candidatura_2018.csv'
+df_candidatura = pd.read_csv(path_file_candidatura, sep=';')
 df_candidatura.head()
 
-# Lendo um xlsx
-pathfile_xlsx = "/home/teo/Documentos/pessoais/projetos/ensino/projetos_twitch/meu_treino/PandasToTeo/data/tb_declaracao_2018.xlsx"
-df_declaracao = pd.read_excel(pathfile_xlsx)
+# Lendo um xlsx:
+path_file_declaracao = '/home/fabiano/Insync/fabiano.moreira.alves@gmail.com/Google Drive/Projects/class_pandas/data/tb_declaracao_2018.xlsx'
+df_declaracao = pd.read_excel(path_file_declaracao)
 df_declaracao.head()
 
-#############################
-# Brincando com o DataFrame...
-#############################
+# Brincando com o DataFrame
 
-# Número de linhas para serem exibidas a partir da primeira
-df_candidatura.head(2) # Isso é um método
+# Número das primeiras linhas a serem exibidas
+df_candidatura.head(2)
 
-# Numero de linhas para serem exibidas a partir da última
-df_candidatura.tail(2) # Isso tbm é um metodo
+# Número das últimas linhas a serem exibidas
+df_candidatura.tail(2)
 
-# Numero de linhas e colunas de um dataframe (tupla)
-df_candidatura.shape[0] # Isso é um atributo
+# Descobrindo o número de linhas no DataFrame (tupla), linhas e colunas
+df_candidatura.shape
 
-# Quais são as colunas do dataframe?? Sabemos que temos 45 colunas
-df_candidatura.columns # Outro atributo
+# Quais são as colunas do dataframe? Sabemos que temos 45 colunas
+df_candidatura.columns
 
-# Navegando pelas colunas do DataFrame...
-df_candidatura['nome'] # Retorna séries com do nome, ou coluna, melhor dizendo
+# Navegando pelas colunas dataframe
+df_candidatura['nome']
+
+df_candidatura[['nome', 'cpf', 'descricao_ocupacao']]
 
 colunas_selecionadas = ['nome', 'cpf', 'descricao_ocupacao']
-df_candidatura[ colunas_selecionadas ].head()
 
-df_candidatura_new = df_candidatura[ colunas_selecionadas ]
+df_candidatura[colunas_selecionadas].head()
 
-# Navegando pelas colunas e linhas do DataFrame...
-df_candidatura["nome"][29140] # df[column][index]
+# Navegando pelas colunas e linhas do Dataframe
+df_candidatura['nome'][29140]  # df[colunm][index]
 
-# Informações do DataFrame...
+# Informações do Dataframe
 df_candidatura.info()
 
-# Navegando apenas nas linhas
-df_candidatura.loc[29140:29145]
-
-# Para casa..
-# Quantas linhas, colunas tem a base de tb_declaracao_2018.xlsx ?
+# Navegando pelas linhas
+df_candidatura.iloc[29140:29145]
